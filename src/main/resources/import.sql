4c47fb5494e5098d6142050e2d506ec686f9e97c
unknown <user@.ustr.com>
2011-04-13 22:05:44 -0400

    Added an import sql script

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into gtdusers (id, username, password, fullname) values(1,'test','password','Full Name')
+insert into gtdcontexts (id, name, gtduser_id) values (1,'Home',1)
+insert into gtdcontexts (id, name, gtduser_id) values (2,'Work',1)
